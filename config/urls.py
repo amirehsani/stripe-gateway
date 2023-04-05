b8f7a532b2dd0b9932a0afb44ac9ba9339528077
Amir <amirmohammad7284@gmail.com>
2023-04-06 00:40:03 +0330

    Installed Stripe and set api_key

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # path('payment/', include('payment.urls')),
+    # path('api/', include('api.urls')),
 
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("schema/", SpectacularAPIView.as_view(api_version="v1"), name="schema"),
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
+import stripe
 
-# Create your views here.
+stripe.api_key = "sk_test_51Mtd4OGEC3VOAORUocKIJKlFQxgCgXLN1BTzWrZPGNgEf9wzDZ0YvqaCK3TI1bfyZWg8cwoYEWgBRdiOwq5qEMAw00VIPa2adA"
