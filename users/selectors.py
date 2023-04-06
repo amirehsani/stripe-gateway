4b6189bcf8a58140dc99428c15161d01368ea1d2
Amir <amirmohammad7284@gmail.com>
2023-04-06 15:01:35 +0330

    Add user selectors

--- users/selectors.py
+++ users/selectors.py
@@ -1,5 +1,9 @@
 """ Selectors are functions that mostly take care of fetching from the database. """
 
+from django.db.models import QuerySet
 
-def get_profile():
-    ...
+from .models import Profile, BaseUser
+
+
+def get_profile(user: BaseUser) -> QuerySet[Profile]:
+    return Profile.objects.get(user=user)
